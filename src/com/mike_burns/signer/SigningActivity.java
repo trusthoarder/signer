225b0b8bc06140adce621b680d6a4f1289923203
Mike Burns <mike@mike-burns.com>
2014-02-26 20:20:01 +0100

    Initial commit

---
+++ src/com/mike_burns/signer/SigningActivity.java
@@ -0,0 +1,15 @@
+package com.mike_burns.signer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SigningActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
