4fbfaa26f9de3d6e8da9edb7c383b8a9e525bfd7
Chad Mazzola <chad.mazzola@klarna.com>
2014-02-26 21:28:05 +0100

    Button and search input.

--- src/com/mike_burns/signer/SigningActivity.java
+++ src/com/mike_burns/signer/SigningActivity.java
@@ -10,6 +10,6 @@ public class SigningActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.search);
     }
 }
